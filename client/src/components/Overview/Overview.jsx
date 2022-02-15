2c84041a0a61763e39d2e887b9c679bfd222ef2c
Alvina Kwon <minjina90@gmail.com>
2022-02-15 15:08:49 -0600

    implemented OverviewContext for overview and related items.

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import Gallery from './Gallery';
-import { useData } from '../DataProvider';
+// import { useData } from '../DataProvider';
 // import css from './Overview.css';
-import appcss from '../App.css';
+// import appcss from '../App.css';
 
 function Overview() {
   return (
     <Gallery />
-    // <>
-    //   <p className={appcss.title}>Title</p>
-    //   <p className={appcss.sub_title}>Sub Title</p>
-    //   <p className={appcss.para_title}>para title</p>
-    //   <p className={appcss.para_md}>para md</p>
-    //   <p className={appcss.para_sm}>para sm</p>
-    //   <p>Default</p>
-    // </>
+
+  // <>
+  //   <p className={appcss.title}>Title</p>
+  //   <p className={appcss.sub_title}>Sub Title</p>
+  //   <p className={appcss.para_title}>para title</p>
+  //   <p className={appcss.para_md}>para md</p>
+  //   <p className={appcss.para_sm}>para sm</p>
+  //   <p>Default</p>
+  // </>
   );
 }
 
