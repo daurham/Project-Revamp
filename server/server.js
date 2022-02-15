9c893fde10941eb2c6fd513b21a379ada6791c9a
Austin Wang <austinthewang@gmail.com>
2022-02-14 22:06:32 -0800

    working on server

--- server/server.js
+++ server/server.js
@@ -14,7 +14,27 @@ const config = { headers };
 
 app.get('/products/:id', (req, res) => {
   axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${req.params.id}`, config)
-    .then((result) => { res.send(result.data); });
+    .then((result) => { res.send(result.data); })
+    .catch(() => res.sendStatus(500));
+});
+
+// --- For Related Items ---
+app.get('/products/:id/related', (req, res) => {
+  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${req.params.id}/related`, config)
+    .then((result) => result.data)
+    .catch(() => res.sendStatus(500));
+});
+
+app.get('/products/:id/relatedinfo', (req, res) => {
+  const axiosrequest1 = axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${req.params.id}/related`, config);
+  const axiosrequest2 = axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${req.params.id}/styles`, config);
+
+  axios.all([axiosrequest1, axiosrequest2])
+    .then(axios.spread((res1, res2) => {
+      // console.log(res1.data);
+      // console.log(res2.data);
+      res.send([res1.data, res2.data]);
+    }));
 });
 
 // app.get('/reviews', (req, res) => {
