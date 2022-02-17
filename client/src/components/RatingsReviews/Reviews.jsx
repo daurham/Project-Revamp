be92c5ce2055fbec3c17c7126cb73df0c23da9cc
Bulgan Erdenebaatar <bulganebaatar@gmail.com>
2022-02-17 13:44:54 -0800

    date formatted to m/d/y

--- client/src/components/RatingsReviews/Reviews.jsx
+++ client/src/components/RatingsReviews/Reviews.jsx
@@ -1,12 +1,31 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import css from './Reviews.css';
 import { useRatingData } from './RatingProvider';
 import { useData } from '../Context/DataProvider';
 import ReviewsList from './ReviewsList';
 
 function Reviews() {
-  const { productId } = useData()
-  const { reviews, getReviews } = useRatingData()
+  const { productId } = useData();
+  const { reviews, getReviews } = useRatingData();
+
+  const [review, setReviews] = useState([]);
+  const [tile, setTile] = useState(2);
+  console.log('reviews', reviews, 'new review', review)
+
+  // const renderTile = () => {
+  //   if (reviews.length !== 0) {
+  //     let arrayRev = reviews.results;
+  //     for (var i = 0; i < arrayRev.length; i++) {
+  //       while (i <= tile) {
+  //        console.log(arrayRev[i])
+  //       }
+  //     }
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   renderTile();
+  // }, [reviews], [productId])
 
   return reviews.length !== 0 ? (
     <div className={css.container}>
@@ -15,8 +34,8 @@ function Reviews() {
           <ReviewsList review={review} key={id} />))}
       </div>
       <div className={css.buttons}>
-          <button>More Reviews</button>
-          <button>Add a Review</button>
+        <button>More Reviews</button>
+        <button>Add a Review</button>
       </div>
     </div>
   ) : <div>Loading</div>
--- client/src/components/RatingsReviews/ReviewsList.jsx
+++ client/src/components/RatingsReviews/ReviewsList.jsx
@@ -3,17 +3,20 @@ import css from './Reviews.css';
 import StarRating from '../SharedComponents/StarRating';
 
 function ReviewsList({review}) {
+  const isoString = new Date().toISOString();
+
+  const options = { month: "long", day: "numeric", year: "numeric" };
+  const date = new Date(review.date);
+  const americanDate = new Intl.DateTimeFormat("en-US", options).format(date);
+
   return (
     <div className={css.container}>
       <StarRating value={review.rating}/>
-      {/* <ul>
-
-      </ul> */}
       <h2 className={css.sub_title}>{review.summary}</h2>
       <p className={css.para_md}>Review: {review.body}</p>
       {/* <p>recommend: {review.recommend}</p> */}
       {/* <p>rating: {review.rating}</p> */}
-      <p className={css.para_sm}>Date: {review.date}</p>
+      <p className={css.para_sm}>Date: {americanDate}</p>
 
     </div>
   )
