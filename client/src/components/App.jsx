26ab4003efa9203a58738cae8fcff17ce9236cd3
Bulgan Erdenebaatar <bulganebaatar@gmail.com>
2022-02-16 20:38:49 -0800

    commit

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Overview from './Overview/Overview';
+import Overview from './Overview/Overview';
 import RelatedItems from './RelatedItems/RelatedItems';
 import QuestAnswers from './QuestAnswers/QuestAnswers';
 import RatingsReviews from './RatingsReviews/RatingsReviews';
