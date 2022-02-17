00677fa323e37e91c2b29576bfaf7c304a2e841e
Bulgan Erdenebaatar <bulganebaatar@gmail.com>
2022-02-16 20:17:23 -0800

    accepted changes in app

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-// import Overview from './Overview/Overview';
+import Overview from './Overview/Overview';
 import RelatedItems from './RelatedItems/RelatedItems';
 import QuestAnswers from './QuestAnswers/QuestAnswers';
 import RatingsReviews from './RatingsReviews/RatingsReviews';
 import DataProvider from './Context/DataProvider';
 import OverviewProvider from './Context/OverviewProvider';
-// import styles from './App.css';
 
 function App() {
   // const { productId } = useData();
@@ -14,7 +13,7 @@ function App() {
 
       <DataProvider>
         <OverviewProvider>
-          {/* <Overview /> */}
+          <Overview />
           <RelatedItems />
         </OverviewProvider>
         <QuestAnswers />
